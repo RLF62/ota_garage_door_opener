c790de547fc02bad613e8917dd346649094a2775
RLF62 <38708952+RLF62@users.noreply.github.com>
2026-07-11 10:32:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import adafruit_simplemath
 # ----------------------------
 # Firmware version / UART updater
 # ----------------------------
-FW_VERSION = "1.0.3-uart-self-recovery"
+FW_VERSION = "1.0.4-uart-self-recovery"
 UPDATE_MODE = False
 _update_expected_size = 0
 _update_expected_checksum = ""
